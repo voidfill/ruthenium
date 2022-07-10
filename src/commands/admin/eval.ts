e34168a111ec2a21b17b9ebaf516d5224c2e43a1
voidfill <71205200+voidfill@users.noreply.github.com>
2022-07-10 02:16:00 +0200

    update eval

--- src/commands/admin/eval.ts
+++ src/commands/admin/eval.ts
@@ -1,5 +1,6 @@
 import { Context, ParsedArgs } from "detritus-client/lib/command";
 import { Permissions } from "detritus-client/lib/constants";
+import { Message } from "detritus-client/lib/structures";
 
 import { BaseCommand } from "../BaseCommand";
 import { CommandClient, Utils, Constants } from "detritus-client";
@@ -31,47 +32,48 @@ export default class MainCommand extends BaseCommand {
 	}
 
 	async run(payload: Context, __args: ParsedArgs): Promise<any> {
-		const { matches } = Utils.regex(
-			Constants.DiscordRegexNames.TEXT_CODEBLOCK,
-			payload.message.content
-		);
+		const { matches } = Utils.regex(Constants.DiscordRegexNames.TEXT_CODEBLOCK, payload.message.content);
 		if (matches.length) {
 			__args.code = matches[0].text;
 		}
 
 		let language = "js";
 		let message: String;
+		let evalRet: any;
 		if (__args.flat) {
 			__args.depth = 0;
 			__args.full = true;
 		}
 		try {
-			message = await Promise.resolve(eval(__args.code));
-			if (!__args.noreply && typeof message === "object") {
-				message = inspect(
-					message,
-					__args.full
-						? {
-								getters: true,
-								depth: __args.depth,
-								showHidden: true,
-						  }
-						: { depth: __args.depth }
-				);
-				language = "json";
-			}
+			evalRet = await Promise.resolve(eval(__args.code));
+			message = inspect(
+				evalRet,
+				__args.full
+					? {
+							getters: true,
+							depth: __args.depth,
+							showHidden: true,
+					  }
+					: { depth: __args.depth }
+			);
+			language = "json";
 		} catch (error: any) {
 			message = error ? error.stack || error.message : error;
 		}
 
-		if (__args.noreply) {
+		if(__args.noreply) {
 			console.log(message);
 			return;
 		}
-		
-		if(message.indexOf(payload.gateway.token) !== -1) {
-			message = message.replaceAll(payload.gateway.token, "TOKEN");
+
+		if (message.constructor == Message.constructor && evalRet?.author?.id == payload.client.user?.id) {
+			return;
 		}
+
+		message = message
+			.replaceAll(payload.commandClient.config.userToken, "TOKEN")
+			.replaceAll(payload.client.user?.email || "", "EMAIL")
+			.replaceAll(payload.client.user?.phone || "", "PHONE");
 
 		const max = 1990 - language.length;
 		return message ? payload.reply(["```" + language, String(message).slice(0, max), "```"].join("\n")) : null;
