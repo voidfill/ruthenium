c4ae01d04155353702a753da928499af067e3218
voidfill <71205200+voidfill@users.noreply.github.com>
2022-08-26 19:45:57 +0200

    switched to real npm package

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # ruthenium
-detritus selfbot
-
-install my modified versions of detritus-client and detritus-client-socket like [this](https://github.com/voidfill/client#detritus-but-fixed-for-selfbots)
+detritus selfbot!!
--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,55 @@ import { readFileSync, writeFileSync } from "fs";
 
 import afterStart from "./afterStart";
 
-
 const config = make(JSON.parse(readFileSync("./config.json", "utf8")));
 const updateConfig = () => {
 	try {
 		writeFileSync("./config.json", JSON.stringify(config.ghost), "utf8");
-	} catch(e) {
-		console.log("Failed to save config file:", e)
+	} catch (e) {
+		console.log("Failed to save config file:", e);
 	}
+};
+
+config.on(Events.SET, () => {
+	updateConfig();
+});
+config.on(Events.DELETE, () => {
+	updateConfig();
+});
+config.on(Events.UPDATE, () => {
+	updateConfig();
+});
+
+const shard = new ShardClient(config.ghost.userToken, {
+	gateway: {
+		identifyProperties: {
+			$browser: "Discord Client",
+			$device: "Computer", //trole
+			$os: "Windows",
+			os: "Windows",
+			browser: "Discord Client",
+			release_channel: "ptb",
+			client_version: "1.0.1017",
+			os_version: "10.0.19043",
+			os_arch: "x64",
+			client_build_number: 143946,
+		},
+		intents: "",
+	},
+	isBot: false
+});
+
+const oldFunc = shard.gateway.getIdentifyData;
+shard.gateway.getIdentifyData = () => {
+	const data = oldFunc.call(shard.gateway);
+	delete data.intents;
+	return data;
 }
 
-config.on(Events.SET, () => {updateConfig()})
-config.on(Events.DELETE, () => {updateConfig()})
-config.on(Events.UPDATE, () => {updateConfig()})
-
-
-const shard = new ShardClient(config.ghost.userToken);
 const commandClient = new CommandClient(shard, {
 	prefix: "..",
 	isBot: false,
+	useClusterClient: false
 });
 commandClient.config = config;
 
